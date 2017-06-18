87b939d42d977381ba1e26483ac99bd61335ad9c
felipe <felipeng.eletrica@gmail.com>
2017-06-18 20:24:20 -0300

    cleanup program remove .elf. directory

--- TestTimers.X/build/default/production/Interrupt.p1.d
+++
@@ -1,6 +0,0 @@
- build/default/production/Interrupt.d  \
- build/default/production/Interrupt.p1:  \
- Interrupt.c  \
-usart.h  \
-comum.h  \
-Interrupt.h 
--- TestTimers.X/build/default/production/comum.p1.d
+++
@@ -1,4 +0,0 @@
- build/default/production/comum.d  \
- build/default/production/comum.p1:  \
- comum.c  \
-comum.h 
